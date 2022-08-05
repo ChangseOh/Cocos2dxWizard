47663b445ff2b42f107ebe6016c0d65c4c48aedd
ChangSe Oh <valpa@naver.com>
2022-08-05 21:17:10 +0900

    simple cocos2dx batch runner

---
+++ Cocos2dxWizard/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Cocos2dxWizard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Tag = "createButton";
+            this.button1.Text = "Create Project";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 535);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Tag = "resetButton";
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseCaptureChanged += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cocos2D-x Tools-Console folder\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Package Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 12);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Language";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Dest Folder";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Tag = "consoleFolderSelect";
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Select";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(103, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "C++",
+            "Java Script",
+            "Lua"});
+            this.comboBox1.Location = new System.Drawing.Point(138, 235);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(210, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Tag = "language";
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.etcCheck);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Tag = "packageName";
+            this.textBox1.Text = "ex)com.mycompany.example";
+            this.textBox1.TextChanged += new System.EventHandler(this.etcCheck);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 282);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Tag = "destinationFolder";
+            this.textBox2.TextChanged += new System.EventHandler(this.etcCheck);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(255, 21);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Tag = "consoleFolder";
+            this.textBox3.TextChanged += new System.EventHandler(this.etcCheck);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Project Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(210, 21);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Tag = "packageName";
+            this.textBox4.TextChanged += new System.EventHandler(this.etcCheck);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 427);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 102);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 412);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 12);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Log";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Force";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Portrait";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(136, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Landscape";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(43, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 58);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(146, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(65, 38);
+            this.pictureBox3.TabIndex = 20;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(77, 323);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 98);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 568);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Cocos2D-X Project Wizard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Cocos2dxWizard/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Cocos2dxWizard
+{
+    public partial class Form1 : Form
+    {
+        Button selTool, selDest, createBtn, resetBtn;
+        ComboBox languageBox;
+        TextBox toolText, projectNameText, packageNameText, destText;
+
+        bool isPortrait = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            //selTool = (Button)
+
+            selTool = this.button3;
+            selDest = this.button4;
+            createBtn = this.button1;
+            resetBtn = this.button2;
+
+            languageBox = this.comboBox1;//(ComboBox)resources.GetObject("language");
+
+            toolText = this.textBox3;
+            projectNameText = this.textBox4;
+            packageNameText = this.textBox1;
+            destText = this.textBox2;
+
+            this.radioButton1.Checked = true;
+            this.radioButton2.Checked = false;
+
+            this.FormClosing += new FormClosingEventHandler(saveSetting);
+
+            loadSetting();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Create
+        {
+            this.richTextBox1.Text = "";
+            //if(toolText!="cocos.bat")
+            bool stop = false;
+            if (toolText.Text.Length <= 0)
+            {
+                stop = true;
+                this.richTextBox1.Text += "Error : No cocos.bat folder\n";
+            }
+            if (packageNameText.Text.Length <= 0)
+            {
+                stop = true;
+                this.richTextBox1.Text += "Error : No Package Name\n";
+            }
+            if (languageBox.SelectedIndex < 0)
+            {
+                stop = true;
+                this.richTextBox1.Text += "Error : Select use Language\n";
+            }
+            if (destText.Text.Length <= 0)
+            {
+                stop = true;
+                this.richTextBox1.Text += "Error : No destination folder\n";
+            }
+            if (projectNameText.Text.Length <= 0)
+            {
+                stop = true;
+                this.richTextBox1.Text += "Error : No Project Name\n";
+            }
+
+            if (stop)
+                return;
+
+            string[] language = { "cpp", "js", "lua" };
+
+            if (toolText.Text.IndexOf("cocos.bat") < 0 && toolText.Text.IndexOf("cocos") < 0)
+                toolText.Text += "cocos.bat";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            
+            if (toolText.Text.Contains("3.3"))
+                isPortrait = false;
+
+            string argu = toolText.Text + " new"
+                + " -p " + packageNameText.Text
+                + " -l " + language[languageBox.SelectedIndex]
+                + " -d " + destText.Text
+                + (isPortrait ? " --portrait" : "")
+                + " " + projectNameText.Text;
+
+            this.richTextBox1.Text = argu + "\n=================\n";
+
+            psi.FileName = "cmd.exe";
+            psi.Arguments = "/C " + argu;
+
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+
+            Process proc = Process.Start(psi);
+
+            while (true)
+            {
+                string txt = proc.StandardOutput.ReadLine();
+                if (txt == null)
+                    break;
+
+                this.richTextBox1.Text += txt;
+                this.richTextBox1.Refresh();
+                Console.WriteLine(txt);
+            }
+
+            Debug.WriteLine("" + psi.Arguments);
+        }
+        private void button2_Click(object sender, EventArgs e)//Reset
+        {
+            toolText.Text = "";
+            packageNameText.Text = "ex)com.mycompany.example";
+            languageBox.SelectedIndex = -1;
+            destText.Text = "";
+            isPortrait = true;
+            projectNameText.Text = "";
+        }
+        private void button3_Click(object sender, EventArgs e)//Sel tools folder
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "cocos.bat|cocos.bat";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                toolText.Text = ofd.FileName;
+            }
+            //Debug.WriteLine("Debug " + sender);
+        }
+        private void button4_Click(object sender, EventArgs e)//Sel dest folder
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                destText.Text = fbd.SelectedPath;
+        }
+        private void etcCheck(object sender, EventArgs e)//Sel dest folder
+        {
+            //Debug.WriteLine("Debug " + languageBox.Items[languageBox.SelectedIndex]);
+            packageNameText.Text = trimmedSpace(packageNameText.Text);
+            destText.Text = trimmedSpace(destText.Text);
+            projectNameText.Text = trimmedSpace(projectNameText.Text);
+        }
+
+        string trimmedSpace(string src)
+        {
+            int start = 0;
+            int num = 0;
+            string tmp = src;
+
+            while(tmp.IndexOf(" ")>0)
+            {
+                num = tmp.IndexOf(" ");
+                string tmp1 = tmp.Substring(0, num);
+                start = num + 1;
+                tmp1 += tmp.Substring(num + 1);
+                tmp = tmp1;
+            }
+
+            return tmp;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            isPortrait = (this.radioButton1.Checked);
+        }
+
+        void saveSetting(object sender, FormClosingEventArgs e)
+        {
+
+            StreamWriter sw = new StreamWriter("./setting.txt");
+
+            sw.WriteLine(toolText.Text);
+            sw.WriteLine(packageNameText.Text);
+            sw.WriteLine(languageBox.SelectedIndex.ToString());
+            sw.WriteLine(destText.Text);
+            sw.WriteLine(isPortrait ? "true" : "false");
+            sw.WriteLine(projectNameText.Text);
+
+            sw.Close();
+
+        }
+        void loadSetting()
+        {
+            if(!File.Exists("./setting.txt"))
+                return;
+
+            StreamReader sr = new StreamReader("./setting.txt");
+            toolText.Text = sr.ReadLine();
+            packageNameText.Text = sr.ReadLine();
+            languageBox.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+            destText.Text = sr.ReadLine();
+            string ip = sr.ReadLine();
+            isPortrait = (ip == "true");
+            projectNameText.Text = sr.ReadLine();
+
+            sr.Close();
+
+            this.radioButton1.Checked = isPortrait;
+            this.radioButton2.Checked = !isPortrait;
+
+        }
+    }
+}
